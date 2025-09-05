ec4b765c09c5db66b6e7485638261d8c416fb76b
thallesborges <dev.thallesborges@gmail.com>
2025-09-05 10:46:40 -0300

    iniciando js com captura dos dados

---
+++ app.js
@@ -0,0 +1,11 @@
+let nome = document.getElementById('nome')
+let email = document.getElementById('email')
+let numero = document.getElementById('numero')
+let senha = document.getElementById('senha')
+
+function verificarEntradas() {
+    console.log(nome.value)
+    console.log(email.value)
+    console.log(numero.value)
+    console.log(senha.value)
+}
